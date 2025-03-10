b42cdbc7b4ce5c96dd5d8746810c3e28e91f4f34
Beatriz Santana <Bsantana2@my.brookdalecc.edu>
2025-03-09 22:51:05 -0400

    added files

---
+++ Client/server/router.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+//Page Listener(router)
+var router = function(app) {
+
+app.get("/", function(req, res){
+    res.status(200).sendFile(path.join(__dirname + "/../Client/home.html"));
+});
+
+app.get("/home", function(req, res){
+    res.status(200).sendFile(path.join(__dirname + "/../Client/home.html"));
+});
+
+app.get("/add_products", function(req, res){
+    res.status(200).sendFile(path.join(__dirname + "/../Client/add_products.html"));
+});
+app.get("/browse", function(req, res){
+    res.status(200).sendFile(path.join(__dirname + "/../Client/browse.html"));
+});
+app.get("/wishlist", function(req, res){
+    res.status(200).sendFile(path.join(__dirname + "/../Client/wishlist.html"));
+});
+}
+
+module.exports = router;
---
+++ Client/server/services.js
@@ -0,0 +1,15 @@
+//const {MongoClient, ObjectId }=require('mongodb');
+
+//const dbUrl =  
+
+//const client = new MongoClient(dbUrl);
+
+
+//var services = function(app) {
+
+    
+
+    
+//module.exports = { services, initializeDatabase };
+    
+    
